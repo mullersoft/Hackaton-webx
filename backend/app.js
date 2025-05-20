af58e3ef9ddfab9777ccd63fffdad3c13b7485d0
mullersoft <mulerselinger@gmail.com>
2025-05-20 22:41:33 +0300

    dotenv is added and db connection is added

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,28 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
 const app = express();
 app.use(express.json());
+dotenv.config({ path: "./config.env" });
+// console.log(process.env.PORT);
+
+//Database connection
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => {
+    console.log("DB connection successful");
+  })
+  .catch((err) => {
+    console.log("DB connection failed", err);
+  });
 
 //Middlewares
 app.use((req, res, next) => {
   console.log(" this is middleware ✔");
   next();
 });
-
+console.log(process.env);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -22,6 +37,7 @@ app.patch("/apiPatch", (req, res) => {
 app.delete("/apiDelete", (req, res) => {
   res.json({ message: "delete api!" });
 });
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
