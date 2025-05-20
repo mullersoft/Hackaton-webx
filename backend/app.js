a35f3b61ce59bc23c9a04bec29142d159b1aef31
mullersoft <mulerselinger@gmail.com>
2025-05-20 13:54:40 +0300

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+//Middlewares
+app.use((req, res, next) => {
+  console.log(" this is middleware ✔");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Hello World!");
+});
+
+app.patch("/apiPatch", (req, res) => {
+  res.json({ message: "patch api!" });
+});
+app.delete("/apiDelete", (req, res) => {
+  res.json({ message: "delete api!" });
+});
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
